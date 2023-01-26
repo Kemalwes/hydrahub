ceebcdb248fe5766002e2e47ffc5d619380917a1
Aliwes <83845664+Kemalwes@users.noreply.github.com>
2023-01-26 03:36:09 +0300

    Update v3.lua

--- v3.lua
+++ v3.lua
@@ -3258,12 +3258,6 @@ function UILibrary.new(gameName, userId, rank)
 
     local Drag = Draggable.Drag(window.MainUI, Frame)
 
-    --// Customize the GUI
-    window.Watermark.Text = ("hydrahub v2 | %s | %s"):format(userId, gameName)
-    local userinfo = window.MainUI.Sidebar.ContentHolder.UserInfo.Content
-    userinfo.Rank.Text = rank
-    userinfo.Title.Text = userId
-
     return setmetatable(
         {
             UI = {},
